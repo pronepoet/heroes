a65d8315f0f33602674a7c6bf70e74b5f9f3c484
Pronepoet <ochiengphillip84@gmail.com>
2022-12-06 01:14:43 +0300

    Add error handling for validations

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,12 @@
 class HeroPowersController < ApplicationController
+    def create
+    heropower = HeroPower.create!(hero_power_params)
+    render json: heropower.hero
+
+    end
+
+    private
+    def hero_power_params
+    params.permit(:strength, :power_id, :hero_id)
+    end
 end
