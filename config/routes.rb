638b43018929ccb7bbcb5bc559a6767029d39d2f
Pronepoet <ochiengphillip84@gmail.com>
2022-12-05 23:44:51 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :hero_powers
-  resources :heroes
-  resources :powers
+  resources :hero_powers, only: :create
+  resources :heroes, only: [:index, :show]
+  resources :powers, only: [:index, :show, :update]
+
 
 end
