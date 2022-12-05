297feb20a6dff17fb365c92d91d00e59b7907239
Pronepoet <ochiengphillip84@gmail.com>
2022-12-05 23:41:31 +0300

    add validations

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,6 @@
 class HeroPower < ApplicationRecord
     belongs_to :hero
     belongs_to :power
+    # validations
+    validates :strength, inclusion: {in: ['Strong', 'Weak', 'Average']}
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,4 +1,7 @@
 class Power < ApplicationRecord
     has_many :heroes, through: :hero_powers
     has_many :hero_powers
+
+    # validations
+    validates :description, length: {minimun: 20}
 end
