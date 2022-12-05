646a0416214a1caf65bbdf2da000c0686fb734b2
Pronepoet <ochiengphillip84@gmail.com>
2022-12-06 00:50:55 +0300

    Add index, show and update to the powers controller and error handling

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,33 @@
 class PowersController < ApplicationController
+    
+    def index
+    powers = Power.all
+    render json: powers
+    end
+    
+    def show
+    power = Power.find_by(id: params[:id])
+    if power
+        render json: power
+    else
+    render json: {error: "Power not found"}, status: :not_found
+    end
+    
+    end
+
+    def update
+    power = Power.find_by(id: params[:id])
+    if power
+        power.update(power_params)
+        render json: power
+    else
+        render json: {error: "Power not found"}, status: :not_found
+    end
+    end
+
+    private
+    def power_params
+    params.permit(:name, :description)
+    end
+
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,7 +1,8 @@
 class Power < ApplicationRecord
-    has_many :heroes, through: :hero_powers
     has_many :hero_powers
+    has_many :heroes, through: :hero_powers
+
 
     # validations
-    validates :description, length: {minimun: 20}
+    validates :description, length: {minimum: 20}
 end
